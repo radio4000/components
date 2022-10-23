373dccd9d1d2a36ec59adb0066b8fc2a382d16db
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2022-10-23 15:46:56 +0200

    improve form comments and outputs for errors with no field specified

--- src/components/r4-form.js
+++ src/components/r4-form.js
@@ -23,6 +23,11 @@ export default class R4Form extends HTMLElement {
 
 	fieldsTemplate = null
 
+	get submitText() {
+		return this.getAttribute('submit-text') || 'Submit'
+	}
+
+	/* all fieldsets' field names attributes (on input, textarea etc.) */
 	get fieldNames() {
 		if (!this.$fieldsets) return []
 		const names = []
@@ -30,10 +35,7 @@ export default class R4Form extends HTMLElement {
 			let fieldName = $fieldset.querySelector('[name]').getAttribute('name')
 			names.push(fieldName)
 		})
-		return names.filter(name => {
-			const inputsNoPrefill = ['submit', 'password', 'confirmation']
-			return inputsNoPrefill.indexOf(name) === -1
-		})
+		return names
 	}
 
 	connectedCallback() {
@@ -54,6 +56,7 @@ export default class R4Form extends HTMLElement {
 		}
 
 		this.$fieldsets = this.querySelectorAll('fieldset')
+
 		this.initialState = this.getInitialState()
 		this.bindFieldsInput(this.$fieldsets)
 		this.render()
@@ -61,79 +64,102 @@ export default class R4Form extends HTMLElement {
 
 	render() {}
 
-	/* get initial config form state, from the URL params || DOM attributes */
+	/*
+		 get initial state for some form fields;
+		 from the URL params || DOM attributes
+	 */
 	getInitialState() {
+		/* the initial state to populate */
+		let state = {}
+
+		/* for some fields we don't want to preffil */
+		const fieldNamesNoPrefill = ['submit', 'password', 'confirmation']
+		const fieldNamesPrefill = this.fieldNames.filter(name => {
+			return fieldNamesNoPrefill.indexOf(name) === -1
+		})
+
+		if (!fieldNamesPrefill) return state
+
+		/* first prefill from the URL query parameters */
 		const urlParams = new URLSearchParams(window.location.search)
-
-		/* the initial config to populate */
-		let config = {}
-
-		if (!this.fieldNames) return config
-
 		if (urlParams) {
 			for (const [paramName, paramValue] of urlParams) {
 
 				/* if the url param is not is the field names list, don't us */
-				if (this.fieldNames.indexOf(paramName) === -1) return
+				if (fieldNamesPrefill.indexOf(paramName) === -1) return
 
 				const value = decodeURIComponent(paramValue)
 				if (value === 'null') {
 				} else if (value) {
-					config[paramName] = value
+					state[paramName] = value
 				}
 			}
 		}
 
-		/* overwrite the URL params generated config, by the DOM attributes */
-		this.fieldNames.forEach(fieldName => {
+		/* overwrite the URL params generated state, by the DOM attributes */
+		fieldNamesPrefill.forEach(fieldName => {
 			const fieldAttributeValue = this.getAttribute(fieldName)
 			if (fieldAttributeValue) {
-				config[fieldName] = fieldAttributeValue
+				state[fieldName] = fieldAttributeValue
 			}
 		})
 
-		return config
+		return state
 	}
 
 	bindFieldsInput($fieldsets) {
 		if (!$fieldsets) return
 
+		/* map of: filed-type <-> field-event-type */
 		const fieldTypes = {
 			input: 'input',
 			textarea: 'input',
 			button: null,
 		}
 
+		/* in all the current form's fieldsets */
 		$fieldsets.forEach($fieldset => {
-			/* listen for changes,
-				 and create only one field(in+out) per fieldset */
+			/*
+				 - find the first matching "input field type" (textarea, input, select, etc.)
+				 - create only one "field output error" per fieldset
+				 - bind field changes, set its initial value (from URL query-parmas || DOM attributes)
+			 */
 			Object.keys(fieldTypes).every(fieldType => {
 				const $field = $fieldset.querySelector(fieldType)
 				const $label = $fieldset.querySelector('label')
 				const fieldEventType = fieldTypes[fieldType]
 
-				if (!$field || !fieldEventType) return true
+				/* if not field for the current field type we're checking for,
+					 break the every loop, to find if there is a field of an other type  */
+				if (!$field) return true
 
+				/* find the name of the "input field" we are dealing with */
 				const fieldName = $field.getAttribute('name')
 
 				const $errorOutput = this.createFieldsetOutput($field)
 				$fieldset.append($errorOutput)
 
+				/* if there is an event type for this field's input:
+					 - bind event for input changes
+					 - set the initial possible state
+				 */
 				if (fieldEventType) {
 					$field.addEventListener(fieldEventType, this.handleInput.bind(this))
-				}
 
-				const initialFieldState = this.initialState[$field.name]
-				if (initialFieldState) {
-					$field.value = initialFieldState
-					$field.dispatchEvent(new Event('input')) // trigger initial value
+					/* find an initial field value */
+					const initialFieldState = this.initialState[$field.name]
+					if (initialFieldState) {
+						$field.value = initialFieldState
+						$field.dispatchEvent(new Event('input')) // trigger initial value
+					}
 				}
 
 				/* add the id on the field "input", to make the label work with minimal markup */
 				if (!$field.getAttribute('id')) {
 					$field.setAttribute('id', fieldName)
 				}
-				if (!$label.getAttribute('for')) {
+
+				if ($label && !$label.getAttribute('for')) {
 					$field.setAttribute('for', fieldName)
 				}
 
