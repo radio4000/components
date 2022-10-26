f65d55ddfba713d78ab0ad065a7b6c69bbe90a4e
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2022-10-26 09:05:52 +0200

    improve r4-actions known actions

--- src/components/r4-actions.js
+++ src/components/r4-actions.js
@@ -32,4 +32,46 @@ export default class R4Actions extends HTMLElement {
 	resetSelect() {
 		this.$select.querySelector('option').selected = true
 	}
+
+	/*
+		 actions, are the value="" attribute,
+		 can be called from outside */
+
+	/* check for known actions this component can handle;
+	 returns true if can handle */
+	checkAction(optionValue) {
+		if (!optionValue) return
+		/* is the command prefixed by "visit-" ? */
+		const visitCommand = optionValue.split('visit-')
+		if (visitCommand.length === 2) {
+			this.visit(visitCommand[1])
+			return true
+		}
+		return false
+	}
+
+	/* visit-{page} */
+	visit(visitAction) {
+		let url
+
+		/* is it a known page */
+		if (visitAction === 'home') {
+			url = `/examples/`
+		} else {
+			/* or a component page */
+			url = `/examples/r4-${visitAction}/`
+		}
+		this.navigate(url)
+	}
+
+	/*
+		 commands,
+		 are the results to actions
+	 */
+
+	/* navigate to a URL in the app;
+ should use the app router (page.js) if any */
+	navigate(url) {
+		window.location.href = url
+	}
 }
