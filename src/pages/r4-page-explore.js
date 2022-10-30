e57242b9584011ae49431bc947facc89eea2c529
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2022-10-30 13:12:07 +0100

    add origin to r4-channels in r4-app

--- src/pages/r4-page-explore.js
+++ src/pages/r4-page-explore.js
@@ -13,7 +13,24 @@ template.innerHTML = `
 `
 
 export default class R4PageHome extends HTMLElement {
+	/* an origin with a {{slug}} token, replaced by the r4-channel,
+		 to link to our app's channel page */
+	get channelOrigin() {
+		if (this.href) {
+			return `/{{slug}}`
+		}
+	}
 	connectedCallback() {
+		this.setupAttributes(template.content)
+		this.render()
+	}
+	setupAttributes(dom) {
+		const $channels = dom.querySelector('r4-channels')
+		if (this.channelOrigin) {
+			$channels.setAttribute('origin', this.channelOrigin)
+		}
+	}
+	render() {
 		this.append(template.content.cloneNode(true))
 	}
 }
