9243ec4a60612e39a6cd72d890d20f3a9fe6e619
oskarrough <oskar@rough.dk>
2023-03-29 14:19:56 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	appType: 'mpa',
+	// appType: 'mpa',
   build: {
+		// https://vitejs.dev/guide/build.html#library-mode
     lib: {
       entry: 'src/index.js',
       formats: ['es'],
+			fileName: 'r4'
     },
     rollupOptions: {
       // external: /^lit/,
