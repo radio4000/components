867d1548a7c5bf0206c897b6a539fe4d05228fcb
oskarrough <oskar@rough.dk>
2023-03-29 14:44:18 +0200

    Fix vite refresh stuff

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// appType: 'mpa',
+	// Enable SPA mode so our /index.html with <r4-app> works across refreshes.
+	appType: 'spa',
   build: {
 		// https://vitejs.dev/guide/build.html#library-mode
     lib: {
