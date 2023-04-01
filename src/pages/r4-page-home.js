28b5b661138e3fa9c40906508f39dabffd8d90df
oskarrough <oskar@rough.dk>
2023-04-01 17:01:42 +0200

    More base styles

--- src/pages/r4-page-home.js
+++ src/pages/r4-page-home.js
@@ -6,24 +6,21 @@ export default class R4PageHome extends LitElement {
 	}
 
 	render() {
-		const authed = this.store.user
 		return html`
-			<p>page: ${this.store.count}</p>
 			<header>
-				Welcome to <r4-title></r4-title>.
-				<br>
-				${authed ? html`<strong>${this.store.user.email}</strong>` : null}
+				<h1><r4-title></r4-title></h1>
+				<p>Welcome to</p>
 			</header>
 			<section>
 				<menu>
 					<li>
-						Start <a href="${this.href}/explore">exploring channels</a> to discover new content.
+						Start <a href="${this.href}/explore">exploring channels</a> to discover new content
 					</li>
 					<li>
-						Select a <r4-title small="true"></r4-title> channel to play its content.
+						Play a <r4-title small="true"></r4-title> channel
 					</li>
 					<li>
-						Create a <a href="/new">new channel</a>.
+						Create a <a href="/new">new channel</a>
 					</li>
 				</menu>
 			</section>
