238b5f1d1caeb7e674b0b1cc1248f8a5653f649e
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2023-04-10 20:42:21 +0200

    test deploy github page

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './',
 	// Enable SPA mode so our /index.html with <r4-app> works across refreshes.
 	appType: 'spa',
-  build: {
+	build: {
 		// https://vitejs.dev/guide/build.html#library-mode
-    lib: {
-      entry: 'src/index.js',
-      formats: ['es'],
-			fileName: 'r4'
-    },
-    rollupOptions: {
-      // external: /^lit/,
-    },
-  },
+		lib: {
+			entry: resolve(__dirname, 'src/index.js'),
+			formats: ['es'],
+			fileName: 'r4',
+			name: 'r4',
+		},
+		rollupOptions: {
+			/* out input file to bundle the js & css */
+			input: {
+				main: resolve(__dirname, 'index.html'),
+			},
+		}
+	},
 })
