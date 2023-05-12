38e408e2a998c47473a4455de2b7be142a2bb34e
oskarrough <oskar@rough.dk>
2023-05-12 17:15:17 +0200

    Install eslint and plugins

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+			// 'eslint:recommended',
+			'plugin:wc/recommended',
+			'plugin:lit/recommended',
+		],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
