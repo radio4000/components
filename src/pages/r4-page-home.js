0bb17dc4d102c211282d356a5da709c2dbb852ac
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2023-05-22 14:28:08 +0200

    use <menu> for homepage lists (try hz scroll)

--- src/pages/r4-page-home.js
+++ src/pages/r4-page-home.js
@@ -8,45 +8,45 @@ export default class R4PageHome extends LitElement {
 
 	render() {
 		return html`
-			<header>
-				${!this.store.user ? html`<h1>Welcome to <r4-title></r4-title>!</h1>` : null}
-			</header>
+			<header>${!this.store.user ? html`<h1>Welcome to <r4-title></r4-title>!</h1>` : null}</header>
 			<main>
 				${this.store.user ? this.renderMenuUser() : this.renderMenuNoUser()}
-				${this.store?.userChannels?.length ? html`
-					<section>
-						<h2>Your channel:</h2>
-						<div class="Grid">
-							${this.store?.userChannels.map((channel) => this.renderChannelCard(channel, this.config.href))}
-						</div>
-					</section>
-				` : null}
-				${this.store?.userChannels?.length ? html`
-					<section>
-						<h2>Your channel follows:</h2>
-						<div class="Grid">
-							${this.store?.followings?.map((channel) => this.renderChannelCard(channel, this.config.href))}
-						</div>
-					</section>
-		` : null}
+				${this.store?.userChannels?.length
+					? html`
+							<section>
+								<h2>Your channel:</h2>
+								<menu>
+									${this.store?.userChannels.map((channel) => this.renderChannelCard(channel, this.config.href))}
+								</menu>
+							</section>
+					  `
+					: null}
+				${this.store?.userChannels?.length
+					? html`
+							<section>
+								<h2>Your channel follows:</h2>
+								<menu>
+									${this.store?.followings?.map((channel) => this.renderChannelCard(channel, this.config.href))}
+								</menu>
+							</section>
+					  `
+					: null}
 			</main>
 		`
 	}
 
 	renderChannelCard(channel) {
 		const channelOrigin = `${this.config.href}/${channel.slug}`
-		return html`<r4-channel-card .channel=${channel} origin=${channelOrigin}></r4-channel-card>`
+		return html` <li>
+			<r4-channel-card .channel=${channel} origin=${channelOrigin}></r4-channel-card>
+		</li>`
 	}
 
 	renderMenuNoUser() {
 		return html`
 			<menu>
-				<li>
-					<a href="${this.config.href}/explore">Explore channels</a> to discover new content
-				</li>
-				<li>
-					<a href=${this.config.href + '/sign/up'}>Sign up</a> to start your own radio
-				</li>
+				<li><a href="${this.config.href}/explore">Explore channels</a> to discover new content</li>
+				<li><a href=${this.config.href + '/sign/up'}>Sign up</a> to start your own radio</li>
 			</menu>
 		`
 	}
