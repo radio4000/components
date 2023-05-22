2f825064539e6ae695d62cbd3a9155cb7a88dd8c
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2023-05-22 13:18:46 +0200

    refactor layout styles

--- src/components/r4-auth-status.js
+++ src/components/r4-auth-status.js
@@ -1,4 +1,4 @@
-import {LitElement, html} from 'lit'
+import { LitElement, html } from 'lit'
 
 /* Authentication Status for the current signed in user;
 	 this componet has two slots, to render something when signed in or out;
@@ -8,13 +8,12 @@ import {LitElement, html} from 'lit'
 
 export default class R4AuthStatus extends LitElement {
 	static properties = {
-		auth: {type: Boolean, reflect: true},
+		auth: { type: Boolean, reflect: true },
 	}
 
 	static shadowRootOptions = { ...LitElement.shadowRootOptions, mode: 'open' }
 
 	render() {
-		// console.log('auth', this.auth)
 		return html`
 			<slot name="in" ?hidden=${!this.auth}></slot>
 			<slot name="out" ?hidden=${this.auth}></slot>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,5 @@
-import {sdk} from '@radio4000/sdk'
+/* this file references all dependencies, external librairies used by the project */
+import { sdk } from '@radio4000/sdk'
 import * as lit from 'lit'
 import page from 'page/page.mjs'
 import radio4000player from 'radio4000-player'
