0f32c17d1cac788db6c3ed4de6955381f96a74dc
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2023-05-22 20:20:09 +0200

    simplify pages sign/home

--- src/pages/r4-page-home.js
+++ src/pages/r4-page-home.js
@@ -10,35 +10,18 @@ export default class R4PageHome extends LitElement {
 		const { href } = this.config
 		const { user, userChannels } = this.store
 		return html`
-			<header>${!this.store.user ? html`<h1>Welcome to <r4-title></r4-title>!</h1>` : null}</header>
 			<menu>
 				<li>
 					<a href=${href + '/explore'}>Explore</a>
 				</li>
 				<li>
-					<r4-auth-status ?auth=${user}>
-						<span slot="in">
-							<a href=${`${href}/settings`}>Settings</a>
-						</span>
-						<span slot="out">
-							<a href=${href + '/sign/up'}>Create new radio</a>
-						</span>
-					</r4-auth-status>
-				</li>
-				<li>
-					<r4-auth-status ?auth=${user}>
-						<span slot="in">
-							<a href=${href + '/sign/out'}>Sign out</a>
-						</span>
-						<span slot="out">
-							<a href=${href + '/sign/in'}>Sign in</a>
-						</span>
-					</r4-auth-status>
+					<a href=${href + '/sign/' + (user ? 'out' : 'in')}>Sign ${user ? 'out' : 'in'}</a>
+					${!user ? html`/ <a href=${this.config.href + '/sign/up'}>up</a>` : null}
 				</li>
 			</menu>
 
 			<main>
-				${this.store.user ? this.renderMenuUser() : this.renderMenuNoUser()}
+				${this.store.user ? this.renderMenuUser() : null}
 				${this.store?.userChannels?.length
 					? html`
 							<section>
@@ -68,15 +51,6 @@ export default class R4PageHome extends LitElement {
 		return html` <li>
 			<r4-channel-card .channel=${channel} origin=${channelOrigin}></r4-channel-card>
 		</li>`
-	}
-
-	renderMenuNoUser() {
-		return html`
-			<menu>
-				<li><a href="${this.config.href}/explore">Explore channels</a> to discover new content</li>
-				<li><a href=${this.config.href + '/sign/up'}>Sign up</a> to start your own radio</li>
-			</menu>
-		`
 	}
 
 	renderMenuUser() {
--- src/pages/r4-page-new.js
+++ src/pages/r4-page-new.js
@@ -2,19 +2,28 @@ import { LitElement, html } from 'lit'
 import page from 'page/page.mjs'
 
 export default class R4PageNew extends LitElement {
+	static properties = {
+		store: { type: Object, state: true },
+		config: { type: Object, state: true },
+	}
+	connectedCallback() {
+		super.connectedCallback()
+		if (!this.store.user) {
+			page(`/sign/in`)
+		}
+		if (this.store.userChannels) {
+			page(`/${this.store.userChannels[0].slug}`)
+		}
+	}
 	render() {
 		return html`
 			<main>
-				<r4-channel-create
-					@submit=${this.onChannelCreate}
-				></r4-channel-create>
+				<r4-channel-create @submit=${this.onChannelCreate}></r4-channel-create>
 			</main>
 		`
 	}
 
-	onChannelCreate({
-		detail: { data }
-	}) {
+	onChannelCreate({ detail: { data } }) {
 		if (data) {
 			page(`/${data.slug}`)
 		}
