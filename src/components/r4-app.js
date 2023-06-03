ab5cdaaf17f1a97928893d2dad5ae6fade4bba41
oskarrough <oskar@rough.dk>
2023-06-03 11:07:22 +0200

    Add /playground page

--- src/components/r4-app.js
+++ src/components/r4-app.js
@@ -338,6 +338,7 @@ function renderRouterCMS({store, config}) {
 			<r4-route path="/settings" page="settings"></r4-route>
 			<r4-route path="/map" page="map"></r4-route>
 			<r4-route path="/search" page="search"></r4-route>
+			<r4-route path="/playground/:color" page="playground"></r4-route>
 			<r4-route path="/:slug" page="channel"></r4-route>
 			<r4-route path="/:slug/update" page="channel-update"></r4-route>
 			<r4-route path="/:slug/player" page="channel-player"></r4-route>
---
+++ src/pages/r4-page-playground.js
@@ -0,0 +1,56 @@
+import {LitElement, html} from 'lit'
+
+export default class R4PagePlayground extends LitElement {
+	// Define the elements properties here.
+	static properties = {
+		// These four are passed down from r4-router.
+		store: {type: Object},
+		config: {type: Object},
+		params: {type: Object},
+		searchParams: {type: Object},
+
+		// Note, reflect is ignored for "state" properties.
+		age: {type: Number, reflect: true}
+	}
+
+	constructor() {
+		// Remember to call super.
+		super()
+
+		// Default values can be set here.
+		this.age = 42
+
+		// In the constructor the element is not yet connected to the DOM,
+		// and we don't have access to any passed down props.
+	}
+
+	async connectedCallback() {
+		// Again, remember to call super.
+		super.connectedCallback()
+		console.log(this)
+		console.log('component:connected', {
+			store: this.store,
+			config: this.config,
+			searchParams: this.searchParams,
+			params: this.params,
+			age: this.age
+		})
+	}
+
+	render() {
+		console.log('component:render')
+		return html`
+			<h1>Playground</h1>
+			<p>params = ${this.params.color}</p>
+			<p>page = ${this.searchParams.get('page')}</p>
+			<p>
+				<a href="/examples/r4-app/playground/yellow?page=666">Update search params with a regular link</a>
+			</p>
+		`
+	}
+
+	// Disable shadow DOM
+	createRenderRoot() {
+		return this
+	}
+}
