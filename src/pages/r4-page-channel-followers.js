b84b43c186e14e4d7fa25fbd8a44f75f5ff7c8b5
oskarrough <oskar@rough.dk>
2023-06-19 09:48:17 +0200

    Fix channel card links

--- src/pages/r4-page-channel-followers.js
+++ src/pages/r4-page-channel-followers.js
@@ -1,7 +1,4 @@
 import {html} from 'lit'
-import {until} from 'lit/directives/until.js'
-import {repeat} from 'lit/directives/repeat.js'
-import {sdk} from '@radio4000/sdk'
 import BaseChannel from './base-channel'
 
 export default class R4PageChannelFollowers extends BaseChannel {
@@ -9,15 +6,12 @@ export default class R4PageChannelFollowers extends BaseChannel {
 		const slug = this.channel?.slug
 		return html`
 			<header>
-				<code>@</code>
-				<a href=${this.channelOrigin}>${slug}</a>
-				<code>/</code>
-				followers &
+				<code>@</code> <a href=${this.channelOrigin}>${slug}</a> <code>/</code> followers &
 				<a href=${this.channelOrigin + '/following'}>following</a>
 			</header>
 			<main>
 				<h1>Channels following ${slug}</h1>
-				<r4-channel-followers slug=${slug}></r4-channel-followers>
+				<r4-channel-followers slug=${slug} href=${this.config.href}></r4-channel-followers>
 			</main>
 		`
 	}
