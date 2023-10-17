78cddfc154c1100f165e1358d233e355bcd8d1f4
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2023-10-17 10:19:11 +0200

    fix: refactor about/yolo{async} naming convention

--- src/pages/r4-page-about.js
+++ src/pages/r4-page-about.js
@@ -7,13 +7,15 @@ export default class R4PageAbout extends R4Page {
 		store: {type: Object, state: true},
 		latestTag: {type: Object, state: true},
 	}
+	roomAlias = '#radio4000:matrix.org'
+	releasesUrl = 'https://github.com/radio4000/components/releases'
 
 	connectedCallback() {
 		super.connectedCallback()
-		this.yolo()
+		this.fetchTheLatestTagFromTheGitProviderRepoUrlAndSetLatestTag()
 	}
 
-	async yolo() {
+	async fetchTheLatestTagFromTheGitProviderRepoUrlAndSetLatestTag() {
 		const owner = 'radio4000'
 		const repo = 'components'
 		const res = await fetch(`https://api.github.com/repos/${owner}/${repo}/tags`)
@@ -28,7 +30,7 @@ export default class R4PageAbout extends R4Page {
 	}
 	renderMain() {
 		return html`
-			<p>Hello. This is going to be the next version of Radio4000.</p>
+			<p>Hello. This is going to be the next version of <r4-title></r4-title>.</p>
 			<p>
 				The current version is still live on
 				<a href="https://radio4000.com">radio4000.com</a>; keep using it normally, until the beta is ready to replace
@@ -41,17 +43,21 @@ export default class R4PageAbout extends R4Page {
 			<p>
 				<strong
 					>Play around, test it out and come say hi in the
-					<a href="https://matrix.to/#/#radio4000:matrix.org" rel="noreferrer">community chat</a>.</strong
+					<a href="https://matrix.to/#/${this.roomAlias}" rel="noreferrer">
+						community chat
+						<small>(${this.roomAlias})</small>
+					</a>
+					.</strong
 				>
 			</p>
 			<p>
-				Contribute to the design and development on <a href="https://github.com/radio4000">github.com/radio4000</a>. The
-				latest version is ${this.latestTag?.name}.
+				Contribute to the design and development on <a href="https://github.com/radio4000">github.com/radio4000</a>.
+			</p>
+			<p>The latest version is <a href=${this.releasesUrl}>${this.latestTag?.name}</a>.</p>
+			<p>Cheers!</p>
+			<p>
+				<a href="${this.config.href}">← back to the home page</a>
 			</p>
 		`
-	}
-
-	createRenderRoot() {
-		return this
 	}
 }
