099549e01dd5a906c4adbc1bb782d28748d2703d
oskarrough <oskar@rough.dk>
2023-10-27 21:06:01 +0200

    Try to reduce theme styles

--- src/libs/appearence.js
+++ src/libs/appearence.js
@@ -1,4 +1,4 @@
-const THEMES = ['default', 'classic', 'jellybeans']
+const THEMES = ['minimal', 'default', 'classic', 'jellybeans']
 const COLOR_SCHEMES = ['os', 'light', 'dark']
 const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
 
