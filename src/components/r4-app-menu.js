94476bd70b6144552e963d8de8f73ddf88dff605
Oskar Rough <oskar@rough.dk>
2023-10-30 13:10:09 +0100

    Add active links to r4-app-menu WIP

--- src/components/r4-app-menu.js
+++ src/components/r4-app-menu.js
@@ -3,6 +3,7 @@ import {LitElement, html} from 'lit'
 export default class R4AppMenu extends LitElement {
 	static properties = {
 		href: {type: String},
+		pathname: {type: String},
 		slug: {type: String},
 		auth: {type: Boolean},
 	}
@@ -12,36 +13,45 @@ export default class R4AppMenu extends LitElement {
 		return this.auth && this.slug
 	}
 
+	get activePath() {
+		const url = new URL(window.location)
+		return url.pathname.replace(this.href.replace(url.origin, ''), '')
+	}
+
 	render() {
+		console.log('RENDER', this.pathname, this.activePath)
 		return html`
 			<menu>
+				<li>hey ${this.pathname}/${this.activePath}</li>
 				<li>
-					<a href=${this.href + '/'}><r4-title size="small"></r4-title></a>
+					<a ?active=${this.activePath === '/'} href=${this.href + '/'}><r4-title size="small"></r4-title></a>
 				</li>
-				<li><a href=${this.href + '/explore'}>Explore</a></li>
-				<li><a href=${this.href + '/map'}>Map</a></li>
-				${!this.auth ? this.renderNoAuth() : this.renderAuth()} ${this.canAdd ? this.renderAdd() : null}
-				<li><a href=${this.href + '/settings'}>Settings</a></li>
+				<li><a ?active=${this.activePath === '/explore'} href=${this.href + '/explore'}>Explore</a></li>
+				<li><a ?active=${this.activePath === '/map'} href=${this.href + '/map'}>Map</a></li>
+				${!this.auth ? this.renderNoAuth() : this.renderAuth()}
+				${this.canAdd ? this.renderAdd() : null}
+				<li><a ?active=${this.activePath === '/settings'} href=${this.href + '/settings'}>Settings</a></li>
 			</menu>
 		`
 	}
 	renderNoAuth() {
 		return html`
-			<li><a href=${this.href + '/sign/up'}>Sign up</a></li>
-			<li><a href=${this.href + '/sign/in'}>Sign in</a></li>
+			<li><a ?active=${this.activePath === '/sign/up'} href=${this.href + '/sign/up'}>Sign up</a></li>
+			<li><a ?active=${this.activePath === '/sign/in'} href=${this.href + '/sign/in'}>Sign in</a></li>
 		`
 	}
 	renderAuth() {
 		if (this.slug) {
-			return html`<li><a href=${this.href + '/' + this.slug}>@${this.slug}</a></li>`
+			return html`<li><a ?active=${this.activePath === `/${this.slug}`} href=${this.href + '/' + this.slug}>@${this.slug}</a></li>`
 		} else {
-			return html`<li><a href=${this.href + '/new'}>New radio</a></li>`
+			return html`<li><a ?active=${this.activePath === '/new'} href=${this.href + '/new'}>New radio</a></li>`
 		}
 	}
 	renderAdd() {
-		return html`<li><a href=${this.href + '/add?slug=' + this.slug}>Add</a></li>`
+		return html`<li><a ?active=${this.activePath === '/add'} href=${this.href + '/add?slug=' + this.slug}>Add</a></li>`
 	}
 	createRenderRoot() {
 		return this
 	}
 }
+
