6d0aff06e555fd8f7db81bbdd76081ae7b45415e
oskarrough <oskar@rough.dk>
2023-11-01 10:45:08 +0100

    Use aria-current

--- src/components/r4-app-menu.js
+++ src/components/r4-app-menu.js
@@ -1,4 +1,4 @@
-import {LitElement, html} from 'lit'
+import {LitElement, html, nothing} from 'lit'
 
 export default class R4AppMenu extends LitElement {
 	static properties = {
@@ -23,38 +23,41 @@ export default class R4AppMenu extends LitElement {
 		return this.auth && this.slug
 	}
 
+	isCurrent(path) {
+		return this.path === path ? 'page' : nothing
+	}
+
 	render() {
 		const {href, path} = this
 		return html`
 			<menu>
 				<li>
-					<a ?active=${path === '/'} href=${href + '/'}><r4-title size="small"></r4-title></a>
+					<a aria-current=${this.isCurrent('/')} href=${href + '/'}><r4-title size="small"></r4-title></a>
 				</li>
-				<li><a ?active=${path === '/explore'} href=${href + '/explore'}>Explore</a></li>
-				<li><a ?active=${path === '/map'} href=${href + '/map'}>Map</a></li>
-				${this.auth ? this.renderAuth() : this.renderNoAuth()}
-				${this.canAdd ? this.renderAdd() : null}
-				<li><a ?active=${path === '/settings'} href=${href + '/settings'}>Settings</a></li>
+				<li><a aria-current=${this.isCurrent('/explore')} href=${href + '/explore'}>Explore</a></li>
+				<li><a aria-current=${this.isCurrent('/map')} href=${href + '/map'}>Map</a></li>
+				${this.auth ? this.renderAuth() : this.renderNoAuth()} ${this.canAdd ? this.renderAdd() : null}
+				<li><a aria-current=${this.isCurrent('/settings')} href=${href + '/settings'}>Settings</a></li>
 			</menu>
 		`
 	}
 	renderNoAuth() {
 		return html`
-			<li><a ?active=${this.path === '/sign/up'} href=${this.href + '/sign/up'}>Sign up</a></li>
-			<li><a ?active=${this.path === '/sign/in'} href=${this.href + '/sign/in'}>Sign in</a></li>
+			<li><a aria-current=${this.isCurrent('/sign/up')} href=${this.href + '/sign/up'}>Sign up</a></li>
+			<li><a aria-current=${this.isCurrent('/sign/in')} href=${this.href + '/sign/in'}>Sign in</a></li>
 		`
 	}
 	renderAuth() {
 		if (this.slug) {
 			return html`<li>
-				<a ?active=${this.path === `/${this.slug}`} href=${this.href + '/' + this.slug}>@${this.slug}</a>
+				<a aria-current=${this.isCurrent(`/${this.slug}`)} href=${this.href + '/' + this.slug}>@${this.slug}</a>
 			</li>`
 		} else {
-			return html`<li><a ?active=${this.path === '/new'} href=${this.href + '/new'}>New radio</a></li>`
+			return html`<li><a aria-current=${this.isCurrent('/new')} href=${this.href + '/new'}>New radio</a></li>`
 		}
 	}
 	renderAdd() {
-		return html`<li><a ?active=${this.path === '/add'} href=${this.href + '/add?slug=' + this.slug}>Add</a></li>`
+		return html`<li><a aria-current=${this.isCurrent('/add')} href=${this.href + '/add?slug=' + this.slug}>Add</a></li>`
 	}
 	createRenderRoot() {
 		return this
