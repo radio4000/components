29f6ba13b51eca443ac12f95eb26872ef6b21318
ug.rp <ugrp@duck.com>
2024-04-18 19:45:58 +0200

    use .env for supabase url and key

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import components from './components/index.js'
 import './index.css'
-
+export {sdk, SUPABASE_URL, SUPABASE_KEY} from "./libs/sdk.js"
 export default {...components}
--- src/libs/sdk.js
+++ src/libs/sdk.js
@@ -1,11 +1,9 @@
 import {createClient} from '@supabase/supabase-js'
 import {createSdk} from '@radio4000/sdk'
 
-// Temporarily hard-coded to the "migration-test" project on Supabase.
-// Yes, this key is meant to be public.
-const key =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxaHdzeGJzamx6Yml3cmVyYXRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1NzE4MjUsImV4cCI6MjAxNDE0NzgyNX0.utV_0mI-k-EcTPlwMFHhNxB0nlvOY-Srx05Mr7EHJcQ'
-const url = 'https://tqhwsxbsjlzbiwreratc.supabase.co'
-const supabase = createClient(url, key)
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 export const sdk = createSdk(supabase)
