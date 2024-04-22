0a63d5c03a2728c6b6cd9dd179b5bad59e7b98ef
Oskar Rough <oskar@rough.dk>
2024-04-22 10:39:32 +0200

    Add currently playing info to player summary

--- src/components/r4-layout.js
+++ src/components/r4-layout.js
@@ -115,7 +115,10 @@ export default class R4Layout extends LitElement {
 		return html`
 			<details open part="playback-details" ${ref(this.detailsRef)}>
 				<summary part="playback-summary">
-					${this.isPlaying ? this.renderPlaybackIcon() : null}
+					${this.isPlaying ?
+						html`${this.renderPlaybackIcon()}
+							<span part="playback-status-title">${this.config.playingChannel?.name} - ${this.config.playingTrack?.title}</span>
+							` : null}
 					<slot name="playback-controls"> ${Object.entries(this.uiStates).map(this.renderUiState.bind(this))} </slot>
 				</summary>
 				<slot name="player"></slot>
