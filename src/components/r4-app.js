7a1fff8f1672e58fe66ce79084c48488f21bdf26
Oskar Rough <oskar@rough.dk>
2024-04-22 14:32:46 +0200

    Prefer versioned theme CSS

--- src/components/r4-app.js
+++ src/components/r4-app.js
@@ -93,7 +93,7 @@ export default class R4App extends LitElement {
 		if (this.cdn?.length > 4) {
 			return `${this.cdn}/dist/themes/${file}`
 		} else if (this.cdn) {
-			return `https://cdn.jsdelivr.net/npm/@radio4000/components@latest/dist/themes/${file}`
+			return `https://cdn.jsdelivr.net/npm/@radio4000/components@${this.version || 'latest'}/dist/themes/${file}`
 		} else {
 			return `/themes/${file}`
 		}
