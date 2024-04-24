a2b4e2d341487304bd7ae52b8503206c186b7f6b
ug.rp <ugrp@duck.com>
2024-04-24 11:10:49 +0200

    update home page text

--- src/pages/r4-page-home.js
+++ src/pages/r4-page-home.js
@@ -12,7 +12,7 @@ export default class R4PageHome extends R4Page {
 	}
 	renderMain() {
 		return html`
-			${this.store.userChannels?.length ? this.renderUserChannels() : this.renderCreateChannel()}
+			${this.store.userChannels?.length ? this.renderUserChannels() : this.renderSignIn()}
 			${this.store.following?.length ? this.renderFollowingChannels() : null}
 		`
 	}
@@ -29,8 +29,13 @@ export default class R4PageHome extends R4Page {
 			</section>
 		`
 	}
-	renderCreateChannel() {
-		return html` <p><a href="${this.config.href}/new">Create radio &rarr;</a></p> `
+	renderSignIn() {
+		return html`
+			<p>
+				<a href="${this.config.href}/sign/in">Sign in</a> a user account, to create, import or manage a radio 4000
+				channel.
+			</p>
+		`
 	}
 	renderFollowingChannels() {
 		const {following} = this.store
@@ -52,15 +57,18 @@ export default class R4PageHome extends R4Page {
 			<section>
 				<r4-favicon></r4-favicon>
 				<dialog open inline>
-					<p>Welcome to the new <r4-title></r4-title>, version 2 (<strong>v2</strong>).</p>
 					<p>
-						All previous radio channels are still available on the
-						<a href="${this.config.hrefV1}">v1</a> website.
+						Welcome to the new <strong><r4-title></r4-title></strong>, version 2 (<strong>v2</strong>).
 					</p>
 					<p>
 						To import a radio channel from v1 to v2, see
 						<a href="${this.config.hrefMigrate}">migrate</a>.
 					</p>
+					<p>
+						All previous radio channels are still available on the
+						<a href="${this.config.hrefV1}">v1</a> website.
+					</p>
+					<p>We hope y'all enjoy, feedbacks' always welcome!</p>
 				</dialog>
 			</section>
 		`
