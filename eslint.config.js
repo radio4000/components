9bff6776c1b76fa6f794a90dd7bd83778a11ee31
Oskar Rough <oskar@rough.dk>
2024-05-18 17:57:19 +0200

    Update eslint to version 9 "flat configs"

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import jsdoc from 'eslint-plugin-jsdoc'
+import pluginLit from 'eslint-plugin-lit'
+import {configs as wcConfigs} from 'eslint-plugin-wc'
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+    	// This can be enabled if we want to clean up unused variables, but it's not necessary.
+      'no-unused-vars': 'off',
+    },
+  },
+  js.configs.recommended, // eslint recommended
+  jsdoc.configs['flat/recommended'], // for jsdoc
+  wcConfigs['flat/recommended'], // for web components
+  pluginLit.configs['flat/recommended'], // for lit elements
+]
