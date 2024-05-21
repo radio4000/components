947d334b1bcc51c1d52e92523caf067de7db4cf7
Oskar Rough <oskar@rough.dk>
2024-05-21 14:56:47 +0200

    Move prettier config to own file

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+	"semi": false,
+	"singleQuote": true,
+	"bracketSpacing": false,
+	"printWidth": 120,
+	"useTabs": true
+}
+
+export default config
+
