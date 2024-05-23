29e29b314172027a5087644e1a24eec7402de950
oskarrough <oskar@rough.dk>
2024-05-23 19:08:58 +0200

    Update fuzzysort dependency

--- src/components/r4-app-menu.js
+++ src/components/r4-app-menu.js
@@ -7,26 +7,35 @@ export default class R4AppMenu extends LitElement {
 	createRenderRoot() {
 		return this
 	}
+
 	static properties = {
 		href: {type: String},
 		path: {type: String, state: true},
 	}
+
 	constructor() {
 		super()
+
+		this.href = ''
+		this.path = ''
+
 		// Set "path" on every navigation.
 		window?.navigation?.addEventListener('navigate', (e) => {
 			this.path = e.destination.url.replace(this.href, '').split('?')[0]
 		})
 	}
+
+	/** @param {string} path */
 	isCurrent(path) {
 		return this.path === path ? 'page' : nothing
 	}
+
 	render() {
-		const {href, path} = this
+		const {href} = this
 		return html`
 			<menu>
 				<li>
-					<a aria-current=${this.isCurrent('/')} href=${href + '/'}>Home</a>
+					<a aria-current=${this.isCurrent('/')} href=${href + '/'} title="Use CTRL/CMD+K to open the command menu">Home</a>
 				</li>
 				<li><a aria-current=${this.isCurrent('/explore')} href=${href + '/explore'}>Explore</a></li>
 				<li><a aria-current=${this.isCurrent('/map')} href=${href + '/map'}>Map</a></li>
