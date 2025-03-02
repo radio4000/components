d2ddef84367161df3bcc6d3a49e29baaae4d242e
4www <4www@duck.com>
2025-03-02 16:32:47 +0100

    add r4-share-{track,channel} handling for navigator.share

--- src/components/r4-share-track.js
+++ src/components/r4-share-track.js
@@ -1,58 +1,160 @@
-import {LitElement, html} from 'lit'
+import { LitElement, html } from 'lit';
 
 export default class R4ShareTrack extends LitElement {
 	static get properties() {
 		return {
-			origin: {type: String},
-			track: {type: Object},
-			href: {type: String},
-		}
+			origin: { type: String },
+			track: { type: Object },
+			href: { type: String },
+			canNavigatorShare: { type: Boolean }
+		};
+	}
+
+	onInputClick(event) {
+		event.target.select();
 	}
 
 	constructor() {
-		super()
-		this.track = {}
+		super();
+		this.track = {};
+		this.canNavigatorShare = !!navigator?.share;
 	}
 
 	get trackOriginUrl() {
-		return `${this.origin}${this.track.id}`
+		return `${this.origin}${this.track.id}`;
 	}
+
 	get shareToR4Url() {
-		return `${this.href}/add?track_id=${this.track.id}`
+		return `${this.href}/add?track_id=${this.track.id}`;
+	}
+
+	shareService() {
+		if (this.canNavigatorShare) {
+			navigator.share({
+				url: this.trackOriginUrl
+			})
+				.catch(console.error);
+		}
+	}
+
+	shareMedia() {
+		if (this.canNavigatorShare) {
+			navigator.share({
+				url: this.track.url
+			})
+				.catch(console.error);
+		}
+	}
+
+	shareDiscogs() {
+		if (this.canNavigatorShare) {
+			navigator.share({
+				url: this.track.discogs_url
+			})
+				.catch(console.error);
+		}
+	}
+
+	buildServiceUrlDom() {
+		if (this.canNavigatorShare) {
+			return html`
+				<fieldset>
+					<button type="button" @click="${this.shareService}">
+						Share <r4-title size="small"></r4-title> URL
+					</button>
+				</fieldset>
+			`;
+		} else {
+			return html`
+				<fieldset>
+					<label for="service_url"><r4-title></r4-title> track URL</label>
+					<input
+						readonly
+						name="service_url"
+						type="url"
+						.value="${this.trackOriginUrl}"
+						@click="${this.onInputClick}"
+					/>
+				</fieldset>
+			`;
+		}
+	}
+
+	buildMediaUrlDom() {
+		if (this.canNavigatorShare) {
+			return html`
+				<fieldset>
+					<button type="button" @click="${this.shareMedia}">
+						Share Media URL
+					</button>
+				</fieldset>
+			`;
+		} else {
+			return html`
+				<fieldset>
+					<label for="media_url">Media URL</label>
+					<input
+						readonly
+						name="media_url"
+						type="url"
+						.value="${this.track.url}"
+						@click="${this.onInputClick}"
+					/>
+				</fieldset>
+			`;
+		}
+	}
+
+	buildDiscogsUrlDom() {
+		if (!this.track.discogs_url) return html``;
+		if (this.canNavigatorShare) {
+			return html`
+				<fieldset>
+					<button type="button" @click="${this.shareDiscogs}">
+						Share Discogs URL
+					</button>
+				</fieldset>
+			`;
+		} else {
+			return html`
+				<fieldset>
+					<label for="discogs_url">Discogs URL</label>
+					<input
+						readonly
+						name="discogs_url"
+						type="url"
+						.value="${this.track.discogs_url}"
+						@click="${this.onInputClick}"
+					/>
+				</fieldset>
+			`;
+		}
+	}
+
+	buildRepostDom() {
+		return html`
+			<fieldset>
+				<span>
+					<a name="r4_add" href="${this.shareToR4Url}">
+						Share on <r4-title></r4-title>
+					</a>
+				</span>
+			</fieldset>
+		`;
 	}
 
 	render() {
 		return html`
 			<form>
-				<fieldset>
-					<label for="track_url">Track URL</label>
-					<input
-						readonly
-						name="track_url"
-						type="url"
-						.value="${this.track.id ? this.trackOriginUrl : this.track.url}"
-						@click="${this.onInputClick}"
-					/>
-				</fieldset>
-				<fieldset>
-					<label for="media_url">Media URL</label>
-					<input readonly name="media_url" type="url" .value="${this.track.url}" @click="${this.onInputClick}" />
-				</fieldset>
-				<fieldset>
-					<label for="media_url">Re-post track</label>
-					<span>
-						<a name="r4_add" href="${this.shareToR4Url}">Add to <r4-title></r4-title> channel</a>
-					</span>
-				</fieldset>
+				${this.buildServiceUrlDom()}
+				${this.buildMediaUrlDom()}
+				${this.buildDiscogsUrlDom()}
+				${this.buildRepostDom()}
 			</form>
-		`
-	}
-
-	onInputClick(event) {
-		event.target.select()
+		`;
 	}
 
 	createRenderRoot() {
-		return this
+		return this;
 	}
 }
